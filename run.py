54d33c711ebb6165586c9d3b4f0673c12ea655f8
denisklopotan <denisklopotan@gmail.com>
2021-12-16 22:10:23 +0000

    Add /home/gitpod/.pyenv/versions/3.8.11/bin/python /workspace/nutrition-calculator/run.py

--- run.py
+++ run.py
@@ -1,14 +1,23 @@
+print("**************************************************************************************") # 86*
 print("Welcome user! This simple 'Nutrition Calculator' helps you add calories and nutritional values of food and returns sum.\n")
 print("You can enter multiple articles of food, one after another. They will be simply called 'Article 1', 'Article 2', etc..\n")
 print("Simply enter values when prompted. Energy in calories (cal); fat, carbohydrate and protein content in grams (g). System used is metric.\n")
+print("**************************************************************************************") # 86*
 
 def article1():
     
     print("Article 1:\n")
-    x = int(input("Enter calories: "))
-    y = int(input("How much fat? "))
-    z = int(input("Carbs? "))
-    w = int(input("And protein? "))
+    while True:
+        try:
+            x = int(input("Enter calories: "))
+            y = int(input("How much fat? "))
+            z = int(input("Carbs? "))
+            w = int(input("And protein? "))
+        except ValueError:
+            print("This is not a number! Please provide correct value")
+            continue
+        else:
+            break
     
     article1.variable = [x, y, z, w]
 
@@ -16,11 +25,18 @@ article1()
 
 def article2():
     
-    print("\nArticle 2:\n")
-    x = int(input("Enter calories: "))
-    y = int(input("How much fat? "))
-    z = int(input("Carbs? "))
-    w = int(input("And protein? "))
+    print("Article 1:\n")
+    while True:
+        try:
+            x = int(input("Enter calories: "))
+            y = int(input("How much fat? "))
+            z = int(input("Carbs? "))
+            w = int(input("And protein? "))
+        except ValueError:
+            print("This is not a number! Please provide correct value")
+            continue
+        else:
+            break
     
     article2.variable = [x, y, z, w]
 
