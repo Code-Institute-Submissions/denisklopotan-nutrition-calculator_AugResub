f1845d914d5827bb2c8078329d3ee1d84752422c
denisklopotan <denisklopotan@gmail.com>
2021-12-17 06:16:46 +0000

    Add docstrings

--- run.py
+++ run.py
@@ -1,12 +1,17 @@
 print("**************************************************************************************") # 86*
 print("Welcome user! This simple 'Nutrition Calculator' helps you add calories and nutritional values of food and returns total sum.\n")
-print("You can enter multiple articles of food, one after another. They will be simply called 'Article 1', 'Article 2', etc..\n")
+print("You can enter only 2 articles of food in this version, in sequence. They will be simply called 'Article 1' & 'Article 2'.\n")
 print("Enter only round number values or zero (0) of following: energy in calories (cal); fat, carbohydrate and protein content in grams (g).\n")
 print("To exit the calculator you can press 'Control + C'. And...dont forget to smile! Its healthy and burns calories too! :D\n")
 print("**************************************************************************************") # 86*
 
+
 def article1():
-    
+    """
+    Gets nutritional value input from the user as integer, int().
+    If other type than int() is submitted while loop raises ValueError
+    and asks user to input correct value of round number.
+    """
     print("\nArticle 1:\n")
     while True:
         try:
@@ -20,12 +25,19 @@ def article1():
         else:
             break
     
+    """
+    Access a variable outside of a function with syntax 'func.variable'
+    Values stored will be used to iterate and calculate sum later on.
+    """
     article1.variable = [x, y, z, w]
 
 article1()
 
+
 def article2():
-    
+    """
+    Same as for article1()
+    """
     print("\nArticle 2:\n")
     while True:
         try:
@@ -43,6 +55,11 @@ def article2():
 
 article2()
 
+"""
+Using zip() function to aggregate elements from two iterables or lists.
+Getting sum of added values and printing nutritional values by index
+with corresponding description inside of f-string.
+"""
 result = zip(article1.variable, article2.variable)
 sum = [x + y for (x, y) in result]
-print(f'\nCalories: {sum[0]}, Fat: {sum[1]}g, Carbohydrates: {sum[2]}g, Protein: {sum[3]}g')
+print(f'\nTotal calories: {sum[0]}, Fat: {sum[1]}g, Carbohydrates: {sum[2]}g, Protein: {sum[3]}g')
