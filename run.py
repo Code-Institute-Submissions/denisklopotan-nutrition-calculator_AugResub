1d7f37b28af5fe841b87b04330606bf82b15ed8f
denisklopotan <denisklopotan@gmail.com>
2021-12-17 00:06:50 +0000

    Add f-string to print sum

--- run.py
+++ run.py
@@ -1,20 +1,20 @@
 print("**************************************************************************************") # 86*
 print("Welcome user! This simple 'Nutrition Calculator' helps you add calories and nutritional values of food and returns sum.\n")
 print("You can enter multiple articles of food, one after another. They will be simply called 'Article 1', 'Article 2', etc..\n")
-print("Simply enter values when prompted. Energy in calories (cal); fat, carbohydrate and protein content in grams (g). System used is metric.\n")
+print("For simplicity enter only round number values. Energy in calories (cal); fat, carbohydrate and protein content in grams (g). System used is metric.\n")
 print("**************************************************************************************") # 86*
 
 def article1():
     
-    print("Article 1:\n")
+    print("\nArticle 1:\n")
     while True:
         try:
             x = int(input("Enter calories: "))
             y = int(input("How much fat? "))
-            z = int(input("Carbs? "))
+            z = int(input("Carbohydrates? "))
             w = int(input("And protein? "))
         except ValueError:
-            print("This is not a number! Please provide correct value")
+            print("This is not a round number! Please provide correct value")
             continue
         else:
             break
@@ -30,7 +30,7 @@ def article2():
         try:
             x = int(input("Enter calories: "))
             y = int(input("How much fat? "))
-            z = int(input("Carbs? "))
+            z = int(input("Carbohydrates? "))
             w = int(input("And protein? "))
         except ValueError:
             print("This is not a number! Please provide correct value")
@@ -44,5 +44,4 @@ article2()
 
 result = zip(article1.variable, article2.variable)
 sum = [x + y for (x, y) in result]
-print('\nYour score is as follows:\n\n[calories, fat(g), carbs(g), protein(g)]')
-print(sum)
+print(f'\nCalories: {sum[0]}, Fat: {sum[1]}g, Carbohydrates: {sum[2]}g, Protein: {sum[3]}g')
